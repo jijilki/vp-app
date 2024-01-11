2b52f952df0a230ad7c056a7a0c57d69b6c600fb
Kakkadathu <jijil.kakkadathu@accenture.com>
2024-01-12 00:56:04 +0530

    Image relative link changed to img s3 links

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -12,19 +12,19 @@ export class ProductsComponent implements OnInit{
   ngOnInit(): void {
     this.slides[0] = {
       id: 0,
-      src: './assets/images/boxes.jpg',
+      src: 'https://vijaypackages.s3.ap-south-1.amazonaws.com/images-export/boxes.jpg',
       title: 'First slide',
       subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
     };
     this.slides[1] = {
       id: 1,
-      src: './assets/images/corrugatedpaper.jpg',
+      src: 'https://vijaypackages.s3.ap-south-1.amazonaws.com/images-export/corrugatedpaper.jpg',
       title: 'Second slide',
       subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     }
     this.slides[2] = {
       id: 2,
-      src: './assets/images/boxes.jpg',
+      src: 'https://vijaypackages.s3.ap-south-1.amazonaws.com/images-export/boxes.jpg',
       title: 'Third slide',
       subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
     }
